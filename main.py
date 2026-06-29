bf3508dd41edcae6d9e00396a3a659672eba3fa1
vitalygl1985-coder <vitalygl1985@gmail.com>
2026-06-29 15:06:47 +0300

    Исправление ошибок при создании пользователя

--- main.py
+++ main.py
@@ -584,17 +584,21 @@ async def web_get_rooms(x_token: Optional[str] = Header(None)):
     conn = get_db_connection(); cur = conn.cursor(cursor_factory=RealDictCursor)
     try:
         if session['role'] == 'admin':
+            # Админ видит: 
+            # 1. Все admin_group (системные)
+            # 2. Только те group/private, где он является участником
             query = """
                 SELECT DISTINCT r.id_room, r.name, r.type, r.created_by,
-                    (SELECT COUNT(*) FROM room_participants WHERE id_room = r.id_room) as participants_count
+                       (SELECT COUNT(*) FROM room_participants WHERE id_room = r.id_room) as participants_count
                 FROM rooms r
                 LEFT JOIN room_participants rp ON r.id_room = rp.id_room
                 WHERE r.id_org = %s::uuid 
-                AND (TRIM(r.type) = 'admin_group' OR rp.id_user = %s)
-                ORDER BY r.name ASC
+                  AND (TRIM(r.type) = 'admin_group' OR rp.id_user = %s)
+                ORDER BY CASE WHEN TRIM(r.type)='admin_group' THEN 1 ELSE 2 END, r.name ASC
             """
             print(session['id_org'])
             cur.execute(query, (str(session['id_org']), session['id_user']))
+
         else:
             # Обычный юзер видит только то, где он участник
             query = """
